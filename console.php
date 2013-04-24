eaaac573a4b72b0a77a03bcf283eb2d0a756ff2a
Thiago Paes <thiago.paes@flexy.com.br>
2013-04-24 11:56:21 -0300

    tirei o sufixo Command, feio pra cacete e não é obrigatorio

--- console.php
+++ console.php
@@ -4,8 +4,8 @@ $autoload = require_once __DIR__ . '/vendor/autoload.php';
 $autoload->add('mrprompt', __DIR__ . '/src');
 
 use Symfony\Component\Console\Application,
-	mrprompt\Command\SambaCommand;
+	mrprompt\Command\Samba;
 
 $console = new Application;
-$console->add(new SambaCommand);
+$console->add(new Samba);
 $console->run();
