815a140b85fc30d68426105e9922e8175f2a92ce
Thiago Paes <mrprompt@gmail.com>
2013-06-19 05:40:06 -0300

    removi o método de instancia do cliente

--- app/Traits/WebClient.php
+++ app/Traits/WebClient.php
@@ -6,11 +6,6 @@
 namespace Traits;
 
 /**
- * @uses Guzzle\Http\Client
- */
-use Guzzle\Http\Client;
-
-/**
  * @author Thiago Paes <mrprompt@gmail.com>
  * @license http://gplv3.fsf.org GPL-3.0+
  */
@@ -20,15 +15,4 @@ trait WebClient
      * @var string
      */
 	protected $base = 'http://www.sambaderaiz.net';
-    
-    /**
-     * 
-     * @return \Guzzle\Http\Client
-     */
-    public function getInstance()
-    {
-        $client = new Client($this->base);
-        
-        return $client;
-    }
 }
